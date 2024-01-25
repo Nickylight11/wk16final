f9b543328059baccea9a1d4a52e32a3fe1b46990
Nickylight11 <nicholas.green513@gmail.com>
2024-01-25 09:36:37 -0500

    updated stylesheet and navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,22 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
 
-const NavBar = () => {
+export default function NavigationBar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/records/bp">RecordBPList</Link>
-        </li>
-        <li>
-          <Link to="/records/cardio">RecordCardioList</Link>
-        </li>
-        <li>
-          <Link to="/records/squat">RecordSquatList</Link>
-        </li>
-      </ul>
-    </nav>
+    <Navbar className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand as={Link} to="/records/bp">
+          Fitness Records
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={NavLink} to="/records/bp">
+              Bench Press Records
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/records/cardio">
+              Cardio Records
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/records/squat">
+              Squat Records
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
-};
-
-export default NavBar;
+}
